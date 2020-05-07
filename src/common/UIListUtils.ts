341e5a2de8fc0a159f7cd152eb3363f9586753b6
ok.codes <ok@ok.codes>
2020-05-07 05:21:35 -0500

    Create UIListUtils.ts

---
+++ src/common/UIListUtils.ts
@@ -0,0 +1,11 @@
+export const getSubheaders = <T extends {}>(array: T[], getGroupKey: (item: T) => string, getItemId: (item: T) => string) => {
+    return array.reduce((acc, cur, i) => {
+        // It's guaranteed that the array is sorted by the field we are interested in.
+        const groupKey = getGroupKey(cur);
+        if (!acc.indexedKeys.includes(groupKey)) {
+            acc.headers[getItemId(cur)] = groupKey;
+            acc.indexedKeys.push(groupKey);
+        }
+        return acc;
+    }, {indexedKeys: [] as string[], headers: {} as { [key: string]: string }}).headers;
+};
