02c558173631b15a21fc435ddf302443031e800c
ok.codes <ok@ok.codes>
2020-05-12 03:24:15 -0500

    Update OperationListTabbedWidget.tsx

--- src/components/OperationListTabbedWidget.tsx
+++ src/components/OperationListTabbedWidget.tsx
@@ -6,6 +6,17 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import {ASkeleton} from "../ASkeleton";
 import {TabPanel} from "./TabPanel";
+import {getSubheaders} from "../common/UIListUtils";
+
+type Props<TModel extends { id: string }> = {
+    items: TModel[]
+    loadMore: { hasMore: boolean, loading: boolean, onClick: () => void, emptyListMessage: string, noMoreItemsMessage: string }
+    cardElement: React.ComponentType<{ model: TModel, onEditClick: () => void, onDeleteClick: () => void }>
+    onEditClick: (model: TModel) => void
+    onDeleteClick: (model: TModel) => void
+    modelName: string
+    getGroupingKey: (model: TModel) => string
+}
 
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
@@ -19,9 +30,22 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
 }));
 
-export const OperationListTabbedWidget = () => {
+const getItemId = <T extends { id: string }>(item: T) => item.id;
+
+export const OperationListTabbedWidget = <TModel extends { id: string }>(
+    {
+        items,
+        loadMore: {hasMore, loading, onClick: onLoadMoreClick, emptyListMessage, noMoreItemsMessage},
+        modelName,
+        cardElement: Card,
+        onDeleteClick,
+        onEditClick,
+        getGroupingKey,
+    }: Props<TModel>) => {
     const classes = useStyles();
     const theme = useTheme();
+    const headers = getSubheaders(items, getGroupingKey, getItemId);
+
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
