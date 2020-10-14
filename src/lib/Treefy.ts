da9f66f7dfdb83e74ad9da97c6f941c6d9c91eab
okcodes <ok@ok.codes>
2020-10-14 05:00:33 -0500

    Encapsulate the `_current` parameter in Treefy.

--- src/lib/Treefy.ts
+++ src/lib/Treefy.ts
@@ -1,13 +1,17 @@
-type TreefyArgs = { stringArray: string[], separator: string, _current?: {} };
+type TreefyArgs = { stringArray: string[], separator: string };
 
 type TreefyObject = { [key: string]: TreefyObject };
 
-export const treefy = ({_current = {}, separator, stringArray}: TreefyArgs) => {
+export const treefy = ({separator, stringArray}: TreefyArgs) => {
+    return treefyInternal({separator, stringArray, _current: {}})
+};
+
+const treefyInternal = ({_current = {}, separator, stringArray}: TreefyArgs & { _current: {} }) => {
     return stringArray.reduce((accumulator, current) => {
         const partsArray = current.split(separator);
         const key = partsArray.shift()?.trim();
         if (!key) return accumulator;
-        const value = partsArray.length ? treefy({
+        const value = partsArray.length ? treefyInternal({
             separator,
             stringArray: [partsArray.join(separator)],
             _current: accumulator[key],
