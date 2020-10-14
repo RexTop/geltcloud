644eefdb38976dc3a4e21e99883172fa36db6811
okcodes <ok@ok.codes>
2020-10-14 02:53:56 -0500

    Simplify example

--- src/views/Playground/Playground.tsx
+++ src/views/Playground/Playground.tsx
@@ -1,129 +1,17 @@
-import React, {CSSProperties} from 'react';
-import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import TreeView from '@material-ui/lab/TreeView';
-import TreeItem, {TreeItemProps} from '@material-ui/lab/TreeItem';
-import Typography from '@material-ui/core/Typography';
-import MailIcon from '@material-ui/icons/Mail';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Label from '@material-ui/icons/Label';
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
-import InfoIcon from '@material-ui/icons/Info';
-import ForumIcon from '@material-ui/icons/Forum';
-import LocalOfferIcon from '@material-ui/icons/LocalOffer';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-import {SvgIconProps} from '@material-ui/core/SvgIcon';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import TreeItem from '@material-ui/lab/TreeItem';
 
-declare module 'csstype' {
-    interface Properties {
-        '--tree-view-color'?: string;
-        '--tree-view-bg-color'?: string;
-    }
-}
-
-type MyStyledTreeItemProps = TreeItemProps & {
-    bgColor?: string;
-    color?: string;
-    labelIcon: React.ElementType<SvgIconProps>;
-    labelInfo?: string;
-    labelText: string;
-};
-
-const useTreeItemStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            color: theme.palette.text.secondary,
-            '&:hover > $content': {
-                backgroundColor: theme.palette.action.hover,
-            },
-            '&:focus > $content, &$selected > $content': {
-                backgroundColor: `var(--tree-view-bg-color, ${theme.palette.grey[400]})`,
-                color: 'var(--tree-view-color)',
-            },
-            '&:focus > $content $label, &:hover > $content $label, &$selected > $content $label': {
-                backgroundColor: 'transparent',
-            },
-        },
-        content: {
-            color: theme.palette.text.secondary,
-            borderTopRightRadius: theme.spacing(2),
-            borderBottomRightRadius: theme.spacing(2),
-            paddingRight: theme.spacing(1),
-            fontWeight: theme.typography.fontWeightMedium,
-            '$expanded > &': {
-                fontWeight: theme.typography.fontWeightRegular,
-            },
-        },
-        group: {
-            marginLeft: 0,
-            '& $content': {
-                paddingLeft: theme.spacing(2),
-            },
-        },
-        expanded: {},
-        selected: {},
-        label: {
-            fontWeight: 'inherit',
-            color: 'inherit',
-        },
-        labelRoot: {
-            display: 'flex',
-            alignItems: 'center',
-            padding: theme.spacing(0.5, 0),
-        },
-        labelIcon: {
-            marginRight: theme.spacing(1),
-        },
-        labelText: {
-            fontWeight: 'inherit',
-            flexGrow: 1,
-        },
-    }),
-);
-
-function StyledTreeItem(props: MyStyledTreeItemProps) {
-    const classes = useTreeItemStyles();
-    const {labelText, labelIcon: LabelIcon, labelInfo, color, bgColor, ...other} = props;
-
-    return (
-        <TreeItem
-            label={
-                <div className={classes.labelRoot}>
-                    <LabelIcon color="inherit" className={classes.labelIcon}/>
-                    <Typography variant="body2" className={classes.labelText}>
-                        {labelText}
-                    </Typography>
-                    <Typography variant="caption" color="inherit">
-                        {labelInfo}
-                    </Typography>
-                </div>
-            }
-            style={{
-                '--tree-view-color': color,
-                '--tree-view-bg-color': bgColor,
-            } as CSSProperties}
-            classes={{
-                root: classes.root,
-                content: classes.content,
-                expanded: classes.expanded,
-                selected: classes.selected,
-                group: classes.group,
-                label: classes.label,
-            }}
-            {...other}
-        />
-    );
-}
-
-const useStyles = makeStyles(
-    createStyles({
-        root: {
-            height: 264,
-            flexGrow: 1,
-            maxWidth: 400,
-        },
-    }),
-);
+const useStyles = makeStyles({
+    root: {
+        height: 240,
+        flexGrow: 1,
+        maxWidth: 400,
+    },
+});
 
 export function Playground() {
     const classes = useStyles();
@@ -131,48 +19,23 @@ export function Playground() {
     return (
         <TreeView
             className={classes.root}
-            defaultExpanded={['3']}
-            defaultCollapseIcon={<ArrowDropDownIcon/>}
-            defaultExpandIcon={<ArrowRightIcon/>}
-            defaultEndIcon={<div style={{width: 24}}/>}
+            defaultCollapseIcon={<ExpandMoreIcon/>}
+            defaultExpandIcon={<ChevronRightIcon/>}
         >
-            <StyledTreeItem nodeId="1" labelText="All Mail" labelIcon={MailIcon}/>
-            <StyledTreeItem nodeId="2" labelText="Trash" labelIcon={DeleteIcon}/>
-            <StyledTreeItem nodeId="3" labelText="Categories" labelIcon={Label}>
-                <StyledTreeItem
-                    nodeId="5"
-                    labelText="Social"
-                    labelIcon={SupervisorAccountIcon}
-                    labelInfo="90"
-                    color="#1a73e8"
-                    bgColor="#e8f0fe"
-                />
-                <StyledTreeItem
-                    nodeId="6"
-                    labelText="Updates"
-                    labelIcon={InfoIcon}
-                    labelInfo="2,294"
-                    color="#e3742f"
-                    bgColor="#fcefe3"
-                />
-                <StyledTreeItem
-                    nodeId="7"
-                    labelText="Forums"
-                    labelIcon={ForumIcon}
-                    labelInfo="3,566"
-                    color="#a250f5"
-                    bgColor="#f3e8fd"
-                />
-                <StyledTreeItem
-                    nodeId="8"
-                    labelText="Promotions"
-                    labelIcon={LocalOfferIcon}
-                    labelInfo="733"
-                    color="#3c8039"
-                    bgColor="#e6f4ea"
-                />
-            </StyledTreeItem>
-            <StyledTreeItem nodeId="4" labelText="History" labelIcon={Label}/>
+            <TreeItem nodeId="1" label="Applications">
+                <TreeItem nodeId="2" label="Calendar"/>
+                <TreeItem nodeId="3" label="Chrome"/>
+                <TreeItem nodeId="4" label="Webstorm"/>
+            </TreeItem>
+            <TreeItem nodeId="5" label="Documents">
+                <TreeItem nodeId="10" label="OSS"/>
+                <TreeItem nodeId="6" label="Material-UI">
+                    <TreeItem nodeId="7" label="src">
+                        <TreeItem nodeId="8" label="index.js"/>
+                        <TreeItem nodeId="9" label="tree-view.js"/>
+                    </TreeItem>
+                </TreeItem>
+            </TreeItem>
         </TreeView>
     );
 }
