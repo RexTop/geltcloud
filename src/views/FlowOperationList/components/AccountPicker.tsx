683f8f02c2ef055bc6f064da50d7a87857f42847
okcodes <ok@ok.codes>
2020-10-16 03:32:14 -0500

    Add tree example to modal.

--- src/views/FlowOperationList/components/AccountPicker.tsx
+++ src/views/FlowOperationList/components/AccountPicker.tsx
@@ -7,10 +7,20 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import {CashAccountModel} from "../../../models/CashAccountModel";
 import {Transition} from "../../../components/common/Transition";
+import {treefy, TreefyObject} from '../../../lib/Treefy';
+import TreeItem from '@material-ui/lab/TreeItem';
+import TreeView from '@material-ui/lab/TreeView';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
-        root: {},
+        dialogRoot: {},
+        treeRoot: {
+            height: 240,
+            flexGrow: 1,
+            maxWidth: 400,
+        },
     }),
 );
 
@@ -20,14 +30,72 @@ type Props = {
     dropDownDataForCashAccounts: CashAccountModel[],
 };
 
+const slashSeparatedValues = [
+    'Bitso / TUSD',
+    'Cash / MXN',
+    'Banamex / Credit',
+    'Mercado Pago',
+    'Cold Wallet / One / LTC 1 (segwit)',
+    'Cold Wallet / One / BTC 1 (native segwit)',
+    'Bitso / ETH',
+    'Santander / Credit',
+    'Banamex / Premia / One',
+    'Banamex / Premia / Two',
+    'Car Cash',
+    'Santander / Debit',
+    'Bitso / LTC',
+    'BBVA / Debit',
+    'Bitso / MXN',
+    'Bitso / BTC',
+    'Cold Wallet / One / ETH 1',
+    'Cold Wallet / One / BTC 1 (segwit)',
+    'Banamex / Maestra',
+    'Cold Wallet / Two / DAI',
+    'Cold Wallet / Two / ETH',
+    'Cold Wallet / LLC / DAI',
+    'Cold Wallet / LLC / ETH',
+];
+
+const Branch = ({tree, baseKey}: { tree: TreefyObject, baseKey: string }) => {
+    if (!Object.keys(tree).length) return null;
+    return (
+        <>
+            {Object.keys(tree).map(key => (
+                <TreeItem key={`${baseKey}-${key}`} nodeId={`${baseKey}-${key}`} label={key}>
+                    {Object.keys(tree[key]).map(subKey =>
+                        <Branch
+                            key={`${baseKey}-${key}-${subKey}`}
+                            tree={{[subKey]: tree[key][subKey]}}
+                            baseKey={`${baseKey}-${key}`}
+                        />)}
+                </TreeItem>
+            ))}
+        </>
+    );
+};
+
+const TreefyExample = () => {
+    const classes = useStyles();
+    const tree = treefy({stringArray: slashSeparatedValues, separator: '/'});
+    return (
+        <TreeView
+            className={classes.treeRoot}
+            defaultCollapseIcon={<ExpandMoreIcon/>}
+            defaultExpandIcon={<ChevronRightIcon/>}
+        >
+            <Branch tree={tree} baseKey={'account-picker-tree'}/>
+        </TreeView>
+    );
+};
+
 export const AccountPicker = ({open, handleClose, dropDownDataForCashAccounts}: Props) => {
     const classes = useStyles();
 
     return (
-        <Dialog open={open} onClose={handleClose} TransitionComponent={Transition} className={classes.root}>
+        <Dialog open={open} onClose={handleClose} TransitionComponent={Transition} className={classes.dialogRoot}>
             <DialogTitle>Account</DialogTitle>
             <DialogContent>
-                xxxxxxxxxxxxxxxxxx
+                <TreefyExample/>
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose} color="primary">
